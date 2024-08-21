37d6a98eddbd91f265900bed000e1a60423e7195
Siarhei Siniak <siarheisiniak@gmail.com>
2024-08-21 18:09:24 +0300

    [~] Refactor

--- docker/backend/serve.sh
+++ docker/backend/serve.sh
@@ -1,4 +1,7 @@
 while true; do
     # python3 -m python.io_atomgroup.soccer.manage collectstatic;
-    gunicorn python.io_atomgroup.soccer.wsgi -b 0.0.0.0;
+    gunicorn \
+        python.io_atomgroup.soccer.asgi \
+        -b 0.0.0.0 \
+        --worker-class uvicorn.workers.UvicornWorker;
 done
