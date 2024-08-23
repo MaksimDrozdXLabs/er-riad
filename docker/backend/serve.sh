41ed8c65ec10896d79c8b9117a736e0c2f0cf95e
Siarhei Siniak <s.siniak@atomgroup.io>
2024-08-23 15:53:40 +0300

    [~] Refactor

--- docker/backend/serve.sh
+++ docker/backend/serve.sh
@@ -1,5 +1,8 @@
 while true; do
     python3 -m python.io_atomgroup.soccer.manage collectstatic --noinput;
+    mkdir -p /app/tmp/backend/ipython
+    ln -sf /app/tmp/backend/ipython ~/.ipython
+    ipython3 profile create
     ln -sf /app/docker/backend/ipython_config.py ~/.ipython/profile_default/ipython_config.py
     gunicorn \
         --reload \
