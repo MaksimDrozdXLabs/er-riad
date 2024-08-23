024213cd2523554b9b40ec6dc71d01791dd926e9
Siarhei Siniak <s.siniak@atomgroup.io>
2024-08-23 16:12:58 +0300

    [~] Refactor

--- python/io_atomgroup/soccer/estimator/tests.py
+++ python/io_atomgroup/soccer/estimator/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestEstimator(TestCase):
+    async def test_socket(self):
+        import socketio
+        import pprint
+
+        async with socketio.async_simple_client.AsyncSimpleClient() as client:
+            await client.connect(
+                'ws://nginx/socket.io',
+            )
+            sid = client.sid
+            response = await client.receive()
+            response = await client.receive()
+            import ipdb
+            ipdb.set_trace()
