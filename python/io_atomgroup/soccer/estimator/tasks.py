29cda7bf896ad8258d9fe40d83d382cba0e30266
Siarhei Siniak <s.siniak@atomgroup.io>
2024-08-26 10:47:42 +0300

    [~] Refactor

--- python/io_atomgroup/soccer/estimator/tasks.py
+++ python/io_atomgroup/soccer/estimator/tasks.py
@@ -9,35 +9,42 @@ logger = logging.getLogger(__name__)
 @celery.shared_task()
 def task_simulate_estimator(
     max_count: Optional[int] = None,
+    delay: Optional[float | int] = None,
 ) -> None:
+
+    if delay is None:
+        delay = 1
 
     if max_count is None:
         max_count = 16
 
+    from python.io_atomgroup.lib.paho import Client
     import paho.mqtt.client
     from django.utils import timezone
     import time
     import json
 
-    client = paho.mqtt.client.Client(
+    client = Client(
         paho.mqtt.client.CallbackAPIVersion.VERSION2
     )
-    client.connect('mqtt')
 
-    done_count = 0
+    with client:
+        client.connect('mqtt')
 
-    logger.info('started')
+        done_count = 0
 
-    while True:
-        if done_count >= max_count:
-            break
+        logger.info('started')
 
-        client.publish(
-            'estimator',
-            json.dumps(dict(
-                a=1,
-                ts=timezone.now().isoformat(),
-            ))
-        )
-        time.sleep(1)
-        done_count += 1
+        while True:
+            if done_count >= max_count:
+                break
+
+            client.publish(
+                'estimator',
+                json.dumps(dict(
+                    a=1,
+                    ts=timezone.now().isoformat(),
+                ))
+            )
+            time.sleep(delay)
+            done_count += 1
