a5b83a8486ca44886653f200ed94a58f80c35f0e
Siarhei Siniak <s.siniak@atomgroup.io>
2024-08-27 12:29:38 +0300

    [~] Refactor

--- python/io_atomgroup/soccer/sio.py
+++ python/io_atomgroup/soccer/sio.py
@@ -1,8 +1,17 @@
 import socketio
 
+SIO_REDIS = 'redis://redis:6379/1'
+
+mgr_pub = socketio.AsyncRedisManager(SIO_REDIS, write_only=True,)
+
+mgr = socketio.AsyncRedisManager(SIO_REDIS)
+
 sio = socketio.AsyncServer(
     async_mode='asgi',
     cors_allowed_origins='*',
+    client_manager=mgr,
+    #logger=True,
+    #engineio_logger=True,
 )
 
 sio_app = socketio.ASGIApp(
