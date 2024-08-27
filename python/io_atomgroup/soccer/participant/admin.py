97e1e6fe9199829235aa0d9b2c0228c88154d502
Siarhei Siniak <s.siniak@atomgroup.io>
2024-08-27 16:09:42 +0300

    [~] Refactor

--- python/io_atomgroup/soccer/participant/admin.py
+++ python/io_atomgroup/soccer/participant/admin.py
@@ -4,6 +4,15 @@ from . import models
 # Register your models here.
 
 class ParticipantAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'score',
+        'updated',
+        'contact',
+        'email',
+    ]
+
     class Meta:
         model = models.Participant
 
