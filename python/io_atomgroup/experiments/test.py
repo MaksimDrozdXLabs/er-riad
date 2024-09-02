f3b9cdad7ff6ff1b1037ce66368bc2afe335a59e
Siarhei Siniak <s.siniak@atomgroup.io>
2024-09-02 19:15:05 +0300

    [~] Refactor

--- python/io_atomgroup/experiments/test.py
+++ python/io_atomgroup/experiments/test.py
@@ -7,105 +7,124 @@ import cv2
 import fastapi
 import fastapi.responses
 
-cap = cv.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc(*"MJPG"))
-cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
-cap.set(cv2.CAP_PROP_EXPOSURE, 250)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-#cap.set(cv2.CAP_PROP_FPS, 30.0)
-
-lock = threading.Lock()
-
-state = dict(
-    frame=None,
-    workers=dict(),
-)
-
-def camera_worker():
-    cv.namedWindow('frame', cv.WINDOW_GUI_NORMAL | cv.WINDOW_AUTOSIZE)
-    try:
-        if not cap.isOpened():
-            print("Cannot open camera")
-        while True:
-            # Capture frame-by-frame
-            ret, frame = cap.read()
-
-            if frame is None:
-                time.sleep(0.1)
-                continue
-
-            cv2.putText(frame, f"{datetime.datetime.now().isoformat()}", (900, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-            # print('\r%s', frame.shape, end='')
-            # if frame is read correctly ret is True
-            if not ret:
-                print("Can't receive frame (stream end?). Exiting ...")
-                break
-            # Our operations on the frame come here
-            #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            gray = frame
-
-            with lock:
-                state['frame'] = cv2.resize(
-                    frame,
-                    (1280 // 2, 720 // 2),
-                    interpolation=cv2.INTER_AREA
-                )
-                # state['frame'] = frame
-
-            # Display the resulting frame
-            cv.imshow('frame', gray)
-            if cv.waitKey(1) == ord('q'):
-                break
-        # When everything done, release the capture
-    finally:
-        cap.release()
-        cv.destroyAllWindows()
-
-def camera_feed():
-    while True:
-        with lock:
-            if state['frame'] is None:
-                frame_bytes = b''
-            else:
-                frame_bytes = cv2.imencode(
-                    ".jpg",
-                    state['frame'],
-                    [cv2.IMWRITE_JPEG_QUALITY, 50, cv2.IMWRITE_JPEG_PROGRESSIVE, 1],
-                )[1].tobytes()
-
-        yield (
-            b'--frame\r\n' +
-            b'Content-Type: image/jpeg\r\n\r\n' +
-            frame_bytes + b'\r\n'
+class Test:
+    def __init__(self):
+        self.state = dict(
+            frame=None,
+            workers=dict(),
         )
-        #time.sleep(1.0)
-        time.sleep(1 / 60)
 
-app = fastapi.FastAPI()
+    def camera_worker(self):
+        state = self.state
+        cap = self.cap
+        lock = self.lock
 
-@app.route("/video_feed")
-def video_feed(*args, **kwargs):
-    # return the response generated along with the specific media
-    # type (mime type)
-    return fastapi.responses.StreamingResponse(
-        camera_feed(),
-        media_type="multipart/x-mixed-replace; boundary=frame"
-    )
+        # cv.namedWindow('frame', cv.WINDOW_GUI_NORMAL | cv.WINDOW_AUTOSIZE)
+        try:
+            if not cap.isOpened():
+                print("Cannot open camera")
+            while True:
+                # Capture frame-by-frame
+                ret, frame = cap.read()
 
-@app.route("/index.html")
-def index(*args, **kwargs):
-    return fastapi.responses.HTMLResponse(r'''
-<html>
-  <head>
-    <title>Pi Video Surveillance</title>
-  </head>
-  <body>
-    <h1>Pi Video Surveillance</h1>
-    <img src="/video_feed">
-  </body>
-</html>
-    ''')
+                if frame is None:
+                    time.sleep(0.1)
+                    continue
 
-state['workers']['cv'] = threading.Thread(target=camera_worker)
-state['workers']['cv'].start()
+                cv2.putText(frame, f"{datetime.datetime.now().isoformat()}", (900, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+                # print('\r%s', frame.shape, end='')
+                # if frame is read correctly ret is True
+                if not ret:
+                    print("Can't receive frame (stream end?). Exiting ...")
+                    break
+                # Our operations on the frame come here
+                #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                gray = frame
+
+                with lock:
+                    state['frame'] = cv2.resize(
+                        frame,
+                        #(1280 // 2, 720 // 2),
+                        (1280, 720),
+                        interpolation=cv2.INTER_AREA
+                    )
+                    # state['frame'] = frame
+
+                # Display the resulting frame
+                # cv.imshow('frame', gray)
+                # if cv.waitKey(1) == ord('q'):
+                #     break
+            # When everything done, release the capture
+        finally:
+            cap.release()
+            # cv.destroyAllWindows()
+
+    def camera_feed(self):
+        state = self.state
+        # cap = self.cap
+        lock = self.lock
+
+        while True:
+            with lock:
+                if state['frame'] is None:
+                    frame_bytes = b''
+                else:
+                    frame_bytes = cv2.imencode(
+                        ".jpg",
+                        state['frame'],
+                        #[cv2.IMWRITE_JPEG_QUALITY, 50, cv2.IMWRITE_JPEG_PROGRESSIVE, 1],
+                        [cv2.IMWRITE_JPEG_QUALITY, 75, cv2.IMWRITE_JPEG_PROGRESSIVE, 1],
+                    )[1].tobytes()
+
+            yield (
+                b'--frame\r\n' +
+                b'Content-Type: image/jpeg\r\n\r\n' +
+                frame_bytes + b'\r\n'
+            )
+            #time.sleep(1.0)
+            time.sleep(1 / 60)
+
+    def video_feed(self, *args, **kwargs):
+        # return the response generated along with the specific media
+        # type (mime type)
+        return fastapi.responses.StreamingResponse(
+            self.camera_feed(),
+            media_type="multipart/x-mixed-replace; boundary=frame"
+        )
+
+    def index(*args, **kwargs):
+        return fastapi.responses.HTMLResponse(r'''
+    <html>
+      <head>
+        <title>Pi Video Surveillance</title>
+      </head>
+      <body>
+        <h1>Pi Video Surveillance</h1>
+        <img src="/juggling/video_feed">
+      </body>
+    </html>
+        ''')
+
+
+    def run(self):
+        self.cap = cv.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc(*"MJPG"))
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
+        self.cap.set(cv2.CAP_PROP_EXPOSURE, 250)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        #cap.set(cv2.CAP_PROP_FPS, 30.0)
+
+        self.lock = threading.Lock()
+
+        self.state['workers']['cv'] = threading.Thread(target=self.camera_worker)
+        self.state['workers']['cv'].start()
+
+        app = fastapi.FastAPI()
+        app.route("/juggling/video_feed")(self.video_feed)
+        app.route("/juggling/index.html")(self.index)
+
+        return app
+
+if __name__ == '__main__':
+    app = Test().run()
